afd96280a619f9a28b31f584c95ccecaaf930d29
Sebastian Roming <sebastian.roming@gmail.com>
2018-08-29 19:05:16 +0200

    xo findings; added xo && ava as dependencies

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
+/* eslint-disable guard-for-in */
 
 const jsonValueReplacer = (obj, val) => {
-  let modifiedObj = {};
+  const modifiedObj = {};
   for (const key in obj) {
     modifiedObj[key] = (typeof obj[key] === 'object') ? jsonValueReplacer(obj[key], val) : val;
   }
